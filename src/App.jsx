3c6f8e69fc21c07b57a8cca61a7f72ba0077bacc
Williamquin72 <williamquinteros506@gmail.com>
2024-05-14 16:31:01 -0600

    Nuevos cambios sigo en 41:28

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/programs";
+import { Analytics } from "@vercel/analytics/react";
 const App=()=> {
   return (
     <div>
