b0ebd701924650d8249f4094bb2fd61c3a117042
Williamquin72 <williamquinteros506@gmail.com>
2024-05-20 13:47:07 -0600

    Nuevo change en responsive

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,10 +6,12 @@ import IMGABOUT01 from '../../assets/About Img 2.jpg'
 const About = () => {
     return (
         <div className="About">
-            <div className="About-Left">
-                <img src={IMGABOUT} alt="" />
-                <img src={IMGABOUT01} alt="" />
+            <div className="About-Left01">
+            <img src={IMGABOUT} alt="" />
 
+            </div>
+            <div className="About-Left02">
+                <img src={IMGABOUT01} alt="" />
             </div>
             <div className="About-Right">
 
